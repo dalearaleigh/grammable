1e8f208ce1d80bcb7f5b60dc4998e4b46ccffaaf
Raleigh Dale <dalearaleigh@gmail.com>
2019-05-13 21:20:53 +0000

    editing and updating grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -18,7 +18,12 @@ class GramsController < ApplicationController
     end 
   end 
 
-
+  def edit 
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank? 
+      render plain: 'Not Found :(', status: :not_found
+    end 
+  end 
 
   def create
     @gram = current_user.grams.create(gram_params)
