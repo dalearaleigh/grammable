dc10a5c2efacc150bf9b27233d1c73027485e409
Raleigh Dale <dalearaleigh@gmail.com>
2019-05-15 19:56:36 +0000

    styling the app

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -70,15 +70,11 @@ class GramsController < ApplicationController
 
   private
 
-  
+
 
   def gram_params
     params.require(:gram).permit(:message, :picture)
   end
 
-
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
 
 end
